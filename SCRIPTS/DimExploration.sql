3abf790b8590030f8f05b64f5eb1f7195926f4de
Ann Amboka (Anna) <amukasahann@gmail.com>
2025-11-20 13:00:53 +0300

    Create DimExploration.sql

---
+++ SCRIPTS/DimExploration.sql
@@ -0,0 +1,7 @@
+
+/*Explore the Dimensions*/
+SELECT DISTINCT Factory FROM Gold.Dim_Factories;
+SELECT DISTINCT ProductName FROM Gold.Dim_Factories;
+SELECT DISTINCT Region FROM Gold.Dim_Factories;
+SELECT DISTINCT Country FROM Gold.Fact_Sales;
+SELECT DISTINCT ShipMode FROM Gold.Fact_Sales
