f8be64a40663cf9b6762a7ecc16d2345cf1a288d
Ann Amboka (Anna) <amukasahann@gmail.com>
2025-11-20 12:59:59 +0300

    Create DBSexploration.sql

---
+++ SCRIPTS/DBSexploration.sql
@@ -0,0 +1,8 @@
+/*Explore all the objects in the Databasse
+--1. Explore the Tables
+--2. Explore the Columns
+--3. Explore the Views*/
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+SELECT * FROM INFORMATION_SCHEMA.columns;
+SELECT * FROM INFORMATION_SCHEMA.Views
