1acf26c97bfe1fc953184b376ceb5ca3c8cf85ac
Ann Amboka (Anna) <amukasahann@gmail.com>
2025-11-20 13:03:44 +0300

    Create MagnitudeAnalysis.sql

---
+++ SCRIPTS/MagnitudeAnalysis.sql
@@ -0,0 +1,41 @@
+/* Magnitude Analysis*/
+--Find Total Sales Per Country
+SELECT Country, SUM(Sales) AS TotalSalesByCountry FROM Gold.Fact_Sales GROUP BY Country ORDER BY TotalSalesByCountry DESC
+
+--Find Total Sales By Region
+SELECT Region, SUM(Sales) AS TotalSalesByRegion FROM Gold.Fact_Sales GROUP BY Region ORDER BY TotalSalesByRegion DESC
+
+--Find Total Sales BY Division
+SELECT
+P.Division,
+SUM(S.Sales) AS TotalSalesByDivison
+FROM Gold.Fact_Sales AS S
+LEFT JOIN Gold.Dim_Products AS P
+ON S.ProductID = P.ProductID
+GROUP BY P.Division
+ORDER BY TotalSalesByDivison DESC
+
+--Find Total Sales BY Factory
+SELECT
+P.Factory,
+SUM(S.Sales) AS TotalSalesByFactory
+FROM Gold.Fact_Sales AS S
+LEFT JOIN Gold.Dim_Products AS P
+ON S.ProductID = P.ProductID
+GROUP BY P.Factory
+ORDER BY TotalSalesByFactory DESC
+
+
+--Find The AVERAGE Cost By Product
+SELECT
+P.ProductName,
+AVG(S.Cost) AS AvgSales
+FROM Gold.Fact_Sales AS S
+LEFT JOIN Gold.Dim_Products AS P
+ON S.ProductID = P.ProductID
+GROUP BY P.ProductName
+ORDER BY AvgSales DESC
+
+--Find Tptal Orders By Country
+SELECT Country, COUNT(OrderID) AS TotalOrdersByCountry FROM Gold.Fact_Sales GROUP BY Country ORDER BY TotalOrdersByCountry DESC
+SELECT Region, COUNT(OrderID) AS TotalOrdersByRegion FROM Gold.Fact_Sales GROUP BY Region ORDER BY TotalOrdersByRegion DESC
