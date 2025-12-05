545026318fe900e039836be889d069dc557bc928
Ann Amboka (Anna) <amukasahann@gmail.com>
2025-12-05 14:23:21 +0300

    RankingAnalysis.sql

--- SCRIPTS/RankingAnalysis.sql
+++ SCRIPTS/RankingAnalysis.sql
@@ -2,17 +2,17 @@
 --Find the top 3 Products
 SELECT TOP 3
 P.ProductName,
-SUM(S.Sales) AS TotalSales,
+SUM(S.Sales*S.Units) AS TotalSales,
 ROW_NUMBER () OVER ( ORDER BY SUM(S.Sales) DESC) AS ProductRank
 FROM Gold.Fact_Sales AS S
 LEFT JOIN Gold.Dim_Products AS P
 ON S.ProductID = P.ProductID
 GROUP BY P.ProductName
-
+;
 --Find the Bottom 3 Products
 SELECT TOP 3
 P.ProductName,
-SUM(S.Sales) AS TotalSales,
+SUM(S.Sales*S.Units) AS TotalSales,
 ROW_NUMBER () OVER ( ORDER BY SUM(S.Sales) ASC) AS ProductRank
 FROM Gold.Fact_Sales AS S
 LEFT JOIN Gold.Dim_Products AS P
