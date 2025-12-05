840afa5fcf5984502fc73b1f495f1a4b312d0b2c
Ann Amboka (Anna) <amukasahann@gmail.com>
2025-12-05 14:22:18 +0300

    MeasuresExplo.sql

--- SCRIPTS/MeasuresExplo.sql
+++ SCRIPTS/MeasuresExplo.sql
@@ -1,27 +1,27 @@
 
 /*Explore the Measures;FINDING THE Key Metrics of our business*/
 --Find the Total Sales
-SELECT SUM(Sales) AS TotalSales FROM Gold.Fact_Sales
---Find the number of customesr
+SELECT SUM(Sales*Units) AS TotalSales FROM Gold.Fact_Sales
+--Find the number of customers
 SELECT DISTINCT COUNT(CustomerID) AS TotalCustomers FROM Gold.Fact_Sales
---Find Total ProducTS
+--Find Total Products
 SELECT COUNT(DISTINCT OrderID) AS TotalProducts FROM Gold.Fact_Sales;
 --Find the Average Cost
-SELECT AVG(Cost) AS AvgCost  FROM Gold.Fact_Sales
+SELECT AVG(Cost*Units) AS AvgCost  FROM Gold.Fact_Sales
 --Find the Total Cost
-SELECT SUM(Cost) AS TotalCost  FROM Gold.Fact_Sales;
+SELECT SUM(Cost*Units) AS TotalCost  FROM Gold.Fact_Sales;
 --Find the Tola Gross Profit
-SELECT SUM(GrossProfit) AS TotalGP  FROM Gold.Fact_Sales
-
-/*Create a Report of all the measures in the buusiness*/
-SELECT 'Total Sales' AS MeasureName, SUM(Sales) AS MeasireValue FROM Gold.Fact_Sales
+SELECT SUM(GrossProfit*Units) AS TotalGP  FROM Gold.Fact_Sales
+;
+/*Create a Report of all the measures in the business*/
+SELECT 'Total Sales' AS MeasureName, SUM(Sales*Units) AS MeasureValue FROM Gold.Fact_Sales
 UNION ALL
 SELECT DISTINCT  'TotalCustomers'AS MeasureName,COUNT(CustomerID) AS MeasireValue FROM Gold.Fact_Sales
 UNION ALL
 SELECT 'TotalProducts'AS MeasureName,COUNT(DISTINCT OrderID) AS MeasireValue  FROM Gold.Fact_Sales
 UNION ALL
-SELECT 'AvgCosT'AS MeasureName,AVG(Cost) AS MeasireValue  FROM Gold.Fact_Sales
+SELECT 'AvgCosT'AS MeasureName,AVG(Cost*Units) AS MeasureValue  FROM Gold.Fact_Sales
 UNION ALL
-SELECT  'TotalCost'AS MeasureName,SUM(Cost) AS MeasireValue  FROM Gold.Fact_Sales
+SELECT  'TotalCost'AS MeasureName,SUM(Cost*Units) AS MeasureValue  FROM Gold.Fact_Sales
 UNION ALL
-SELECT 'TotalGP' AS MeasureName,SUM(GrossProfit) AS MeasireValue  FROM Gold.Fact_Sales
+SELECT 'TotalGP' AS MeasureName,SUM(GrossProfit*Units) AS MeasureValue  FROM Gold.Fact_Sales
